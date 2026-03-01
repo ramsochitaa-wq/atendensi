50e27afdc14cb9114ed4fddca58a1eed83f0e3dc
ramsochitaa-wq <ramsochitaa@gmail.com>
2026-03-01 11:29:32 +0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'atendensi-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  // Ongeza files zingine kama CSS au JS external kama unazitumia
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
